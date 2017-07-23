b841ad7e531c3f8fe965369a87b21d5578629990
Dariusz Kluska <darkenk@gmail.com>
2017-07-23 11:37:04 +0200

    Add global cmake and fixes for build env
    Signed-off-by: Dariusz Kluska <darkenk@gmail.com>

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -9,7 +9,7 @@ set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
 # specify the cross compiler
 set(CMAKE_C_COMPILER $ENV{HOME}/bin/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER $ENV{HOME}/bin/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
-set(CMAKE_SYSROOT ${CMAKE_CURRENT_LIST_DIR}/rootfs/)
+set(CMAKE_SYSROOT ${CMAKE_CURRENT_LIST_DIR}/../../../rootfs/)
 
 set(CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath-link,${CMAKE_SYSROOT}/lib/${CMAKE_LIBRARY_ARCHITECTURE}:${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}" CACHE INTERNAL "")
 set(CMAKE_MODULE_LINKER_FLAGS "-Wl,-rpath-link,${CMAKE_SYSROOT}/lib/${CMAKE_LIBRARY_ARCHITECTURE}:${CMAKE_SYSROOT}/usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}" CACHE INTERNAL "")
@@ -18,8 +18,7 @@ set(CMAKE_MODULE_LINKER_FLAGS "-Wl,-rpath-link,${CMAKE_SYSROOT}/lib/${CMAKE_LIBR
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}
     ${CMAKE_SYSROOT})
 
-# search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
